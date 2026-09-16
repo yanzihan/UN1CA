2507a3134f9658af899d06cf0ecc95bd6fff961e
yanzihan <1816135234@qq.com>
2026-09-17 02:58:12 +0800

    Initialize support for P3Q.

---
+++ target/p3q/sff.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2026 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy S21 Ultra 5G (Snapdragon) (p3q)
+
+# Enable AOD live clock
+SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM=activeclock=7,aodversion=7,clocktransition,coverboldfont
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
