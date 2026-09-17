3efa48671b2a1461ce1b8a0e4d4d222ca8d79fb3
yanzihan <1816135234@qq.com>
2026-09-18 01:00:41 +0800

    Initialize support for Y2Q.

---
+++ target/y2q/sff.sh
@@ -0,0 +1,16 @@
+# Copyright (c) 2026 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy S20+ 5G (Snapdragon) (y2q)
+
+# Enable AOD live clock
+SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM=activeclock=7,aodversion=7,clocktransition,coverboldfont
+
+# Set default HFR Refresh Rate
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_DEFAULT_REFRESH_RATE=120
+
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
