6c81cd3e600bfea55d00074eb6bb7638a7e4eb06
yanzihan <1816135234@qq.com>
2026-09-27 01:37:41 +0800

    Fixed an issue where 120Hz could not be enabled.

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -66,8 +66,13 @@ if $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
             "$MODPATH/ead_resolution_legacy/SecSettings.apk/0001-Add-Adaptive-color-tone-feature.patch"
     fi
 else
-    APPLY_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
-        "$MODPATH/ead/SecSettings.apk/0001-Add-Adaptive-color-tone-feature.patch"
+    if [[ "$TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL" == "false" ]]; then
+        APPLY_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
+            "$MODPATH/ead/SecSettings.apk/0001-Add-Adaptive-color-tone-feature-non-DR.patch"
+    else
+        APPLY_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
+            "$MODPATH/ead/SecSettings.apk/0001-Add-Adaptive-color-tone-feature.patch"
+    fi
 fi
 APPLY_PATCH "system" "system/priv-app/SettingsProvider/SettingsProvider.apk" \
     "$MODPATH/ead/SettingsProvider.apk/0001-Add-Adaptive-color-tone-feature.patch"
