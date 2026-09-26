eb42e5b06bf24e16556562c15e72255419afbb81
yanzihan <1816135234@qq.com>
2026-09-27 01:49:05 +0800

    un1ca:Add the KernelSU app for y2q.

--- unica/mods/preload/customize.sh
+++ unica/mods/preload/customize.sh
@@ -1,3 +1,28 @@
+# KernelSU Next
+KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.3.0/KernelSU_Next_v3.3.0_33214-release.apk"
+# https://github.com/tiann/KernelSU/issues/886
+APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
+
+LOG "- Adding KernelSU-Next.apk to preload apps"
+mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
+DOWNLOAD_FILE "$KERNELSU_MANAGER_APK" "$WORK_DIR/system/$APK_PATH"
+
+while IFS= read -r i; do
+    i="${i//$WORK_DIR\/system\//}"
+
+    if [ -d "$WORK_DIR/system/$i" ]; then
+        SET_METADATA "system" "$i" 0 0 755 "u:object_r:system_file:s0"
+    else
+        SET_METADATA "system" "$i" 0 0 644 "u:object_r:system_file:s0"
+    fi
+
+    if [[ "$i" == *".apk" ]] && \
+            ! grep -q "$i" "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"; then
+        LOG "- Adding \"$i\" to /system/system/etc/vpl_apks_count_list.txt"
+        EVAL "echo \"$i\" >> \"$WORK_DIR/system/system/etc/vpl_apks_count_list.txt\""
+    fi
+done <<< "$(find "$WORK_DIR/system/system/preload")"
+
 # Samsung Internet Browser
 # https://play.google.com/store/apps/details?id=com.sec.android.app.sbrowser
 LOG "- Downloading Samsung Internet app"
