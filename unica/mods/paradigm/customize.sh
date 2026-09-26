af0608bf0dcf6306ab704bd8f0230a56ecada998
yanzihan <1816135234@qq.com>
2026-09-27 01:39:16 +0800

    Fixes for some compilation errors

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -180,6 +180,11 @@ LOG "- Downloading latest Game Booster app"
 DOWNLOAD_FILE "$(GET_GALAXY_STORE_DOWNLOAD_URL "com.samsung.android.game.gametools")" \
     "$WORK_DIR/system/system/priv-app/GameTools_Dream/GameTools_Dream.apk"
 
+# Apps crashing due to debloat
+LOG_STEP_IN "- Fixing app crashes"
+ADD_TO_WORK_DIR "$SRC_DIR/prebuilts/extras" "system" "system/etc/permissions"
+LOG_STEP_OUT
+
 # Pet Detector in Galaxy AI
 LOG_STEP_IN "- Adding Pet Detector support in Galaxy AI features"
 if [ -d "$WORK_DIR/vendor/etc/petdetector/studio_pd" ]; then
